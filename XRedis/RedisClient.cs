50b1b25b36854e0c8cf406d34dc3407ae3896a33
AESCR <1017530942@qq.com>
2021-01-27 23:48:53 +0800

    init

---
+++ XRedis/RedisClient.cs
@@ -0,0 +1,114 @@
+﻿#region << 版 本 注 释 >>
+/*----------------------------------------------------------------
+// Copyright (C) 2017 单位 运管家
+// 版权所有。 
+//
+// 文件名：RedisClient
+// 文件功能描述：
+//
+// 
+// 创建者：名字 AESCR
+// 时间：2021/1/27 16:10:55
+//
+// 修改人：
+// 时间：
+// 修改说明：
+//
+// 修改人：
+// 时间：
+// 修改说明：
+//
+// 版本：V1.0.0
+//----------------------------------------------------------------*/
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace XRedis
+{
+    public class RedisClient:IDisposable
+    {
+        RedisSocket socket;
+        private bool disposedValue;
+        private bool disposedValue1;
+
+        public string Host { get; private set; }
+        public int Port { get; private set; }
+        public int SendTimeout { get; set; }
+        public string Password { get; set; }
+        public event EventHandler Connected;
+        public RedisClient(string host= "localhost", int port= 6379,string password="")
+        {
+            if (host == null)
+                throw new ArgumentNullException("host");
+            Port = port;
+            Host = host;
+            if (!string.IsNullOrEmpty(password))
+            {
+                Password = password;
+            }
+        }
+
+        public bool SetNx(string key, string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Expire(string key, in TimeSpan timeSpan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HSetNx(string key, string filed, string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string HMSet(string key, Dictionary<string, string> result)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// 执行Redis命令
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        bool SendCommand(string cmd, params object[] args)
+        {
+            return false;
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue1)
+            {
+                if (disposing)
+                {
+                    // TODO: 释放托管状态(托管对象)
+                    socket.Close();
+                    socket = null;
+                }
+
+                // TODO: 释放未托管的资源(未托管的对象)并替代终结器
+                // TODO: 将大型字段设置为 null
+                disposedValue1 = true;
+            }
+        }
+
+        // // TODO: 仅当“Dispose(bool disposing)”拥有用于释放未托管资源的代码时才替代终结器
+        ~RedisClient()
+        {
+            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ XRedis/RedisSocket.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace XRedis
+{
+    internal class RedisSocket
+    {
+        Socket socket;
+        BufferedStream bstream;
+        private string Host;
+        private int Port;
+        private int SendTimeout;
+
+        void Connect()
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.NoDelay = true;
+            socket.SendTimeout = SendTimeout;
+            socket.Connect(Host, Port);
+            if (!socket.Connected)
+            {
+                socket.Close();
+                socket = null;
+                return;
+            }
+            bstream = new BufferedStream(new NetworkStream(socket), 16 * 1024);
+        }
+        /// <summary>
+        /// redis命令发送格式 https://segmentfault.com/a/1190000011145207
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public bool SendCommand(string cmd, params object[] args)
+        {
+            if (socket==null)
+            {
+                Connect();
+            }
+            if (socket == null)
+                return false;
+            string resp= "*" + (1 + args.Length)+Environment.NewLine;
+            resp += "$" + cmd.Length + Environment.NewLine + cmd + Environment.NewLine;
+            foreach (object arg in args)
+            {
+                string argStr = arg.ToString();
+                int argStrLength = Encoding.UTF8.GetByteCount(argStr);
+                resp += "$" + argStrLength + Environment.NewLine + argStr + Environment.NewLine;
+            }
+            byte[] r = Encoding.UTF8.GetBytes(resp);
+            try
+            {
+                socket.Send(r);
+            }
+            catch
+            {
+                socket.Close();
+                socket = null;
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 解析redis回类型
+        /// </summary>
+        public void ParseResponse()
+        {
+            int c = bstream.ReadByte();
+            switch (c)
+            {
+                // 状态回复
+                case '+':
+                    break;
+                // 错误回复
+                case '-':
+                    break;
+                // 整数回复
+                case ':':
+                    break;
+                // 批量回复
+                case '$': // $后面跟数据字节数(长度)
+                    break;
+                // 多条批量回复
+                case '*': // *表示后面有多少个参数
+                    break;
+            }
+        }
+
+        public void Close()
+        {
+            if (socket!=null)
+            {
+                socket.Close();
+                socket = null;
+            }
+        }
+    }
+}
