bafab3a7ab1ec4673b54e775f24f30a689e2a078
AESCR <1017530942@qq.com>
2021-02-19 12:35:42 +0800

    init

--- RedisClient/KetamaNodeLocator.cs
+++ RedisClient/KetamaNodeLocator.cs
@@ -32,6 +32,7 @@ namespace XRedis
     {
         private SortedList<long, string> ketamaNodes = new SortedList<long, string>();
 
+        private SortedList<long, string> oldKetamaNodes = new SortedList<long, string>();
         /// <summary>
         /// 一致性哈希的实现
         /// </summary>
@@ -39,6 +40,27 @@ namespace XRedis
         /// <param name="nodeCopies">虚拟节点的个数</param>
         public KetamaNodeLocator(List<string> nodes, int nodeCopies)
         {
+           RefreshNodes(nodes,nodeCopies);
+        }
+
+        /// <summary>
+        /// 一致性哈希的实现
+        /// </summary>
+        /// <param name="nodes">实际节点</param>
+        public KetamaNodeLocator(List<string> nodes)
+            : this(nodes, 10)
+        {
+        }
+        /// <summary>
+        /// 对应所有节点
+        /// </summary>
+        /// <param name="nodes">实际节点</param>
+        /// <param name="nodeCopies">虚拟节点的个数</param>
+        public void RefreshNodes(List<string> nodes, int nodeCopies)
+        {
+            oldKetamaNodes = new SortedList<long, string>(ketamaNodes.ToDictionary(x=>x.Key,x=>x.Value));
+            ketamaNodes = null;
+            ketamaNodes=new SortedList<long, string>();
             //对所有节点，生成nCopies个虚拟结点
             foreach (string node in nodes)
             {
@@ -57,15 +79,25 @@ namespace XRedis
             }
         }
 
-        /// <summary>
-        /// 一致性哈希的实现
-        /// </summary>
-        /// <param name="nodes">实际节点</param>
-        public KetamaNodeLocator(List<string> nodes)
-            : this(nodes, 10)
+        public string GetOldNodeForKey(long hash)
         {
+            string rv;
+            long key = hash;
+            //如果找到这个节点，直接取节点，返回
+            if (!oldKetamaNodes.ContainsKey(key))
+            {
+                //得到大于当前key的那个子Map，然后从中取出第一个key，就是大于且离它最近的那个key 说明详见: http://www.javaeye.com/topic/684087
+                var tailMap = from coll in oldKetamaNodes
+                              where coll.Key > hash
+                    select new { coll.Key };
+                if (tailMap == null || tailMap.Count() == 0)
+                    key = oldKetamaNodes.FirstOrDefault().Key;
+                else
+                    key = tailMap.FirstOrDefault().Key;
+            }
+            rv = ketamaNodes[key];
+            return rv;
         }
-
         /// <summary>
         /// 通过Key值获取节点
         /// </summary>
@@ -99,6 +131,17 @@ namespace XRedis
         {
             byte[] digest = HashAlgorithm.ComputeMd5(key);
             string rv = GetNodeForKey(HashAlgorithm.Hash(digest, 0));
+            return rv;
+        }
+        /// <summary>
+        /// 查找Key值以前的节点的位置
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string GetOldNodes(string key)
+        {
+            byte[] digest = HashAlgorithm.ComputeMd5(key);
+            string rv = GetOldNodeForKey(HashAlgorithm.Hash(digest, 0));
             return rv;
         }
     }
