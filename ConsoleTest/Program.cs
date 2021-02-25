8e768e421173ac30dbbd2b21a301f193a03295ca
AESCR <1017530942@qq.com>
2021-02-25 17:53:20 +0800

    init

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -10,30 +10,8 @@ namespace ConsoleTest
     {
         private static void Main(string[] args)
         {
-            RandomNum randomNum = new RandomNum();
-            RedisClusterClient redisCluster = new RedisClusterClient();
-            Task.Run(() =>
-            {
-                while (true)
-                {
-                    redisCluster.GetClusterStatus();
-                    Thread.Sleep(5000);
-                }
-            });
-            int i = 0;
-            while (true)
-            {
-                try
-                {
-                    var index = randomNum.GenerateCheckCodeNum(10);
-                    var randomStr = "aescr" + index;
-                    var redisClient = redisCluster.Set(randomStr, "100");
-                    i++;
-                }
-                catch (Exception e)
-                {
-                }
-            }
+            Console.WriteLine("AESCR");
+            Console.ReadKey();
         }
     }
 }
